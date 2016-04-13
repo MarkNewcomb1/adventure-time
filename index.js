0a28067b34cb12ad74391b5bd9eb72365cb5ff44
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-13 16:40:28 -0600

    button works, math put into function

--- index.js
+++ index.js
@@ -1,61 +1,56 @@
-/*
-	Below is a small script which generates random adventures. There are two
-	problems which are marked below with "TODO". Your assignment is to solve
-	those TODOs and get this script really up to snuff.
-*/
-
 var monsters = [
 	"dragon",
 	"troll",
 	"alien mothership",
-	"illithid"
+	"illithid", 
+    "dachshund"
 ];
 
 var weapons = [
 	"deadly dagger",
 	"artful axe",
 	"skillful swordplay",
-	"menacing mace"
+	"menacing mace",
+    "wet string"
 ];
 
 var actions = [
 	"defeated",
 	"attacked",
 	"ran from",
-	"talked to"
+	"talked to",
+    "did some light tax accounting for"
 ];
 
 var heroGender = Math.random() > 0.5 ? "hero" : "heroine";
+var button = document.getElementById("new_adventure");
+
 
 /*
 	TODO: The following three lines of code have a lot of repetition and are
 	ripe for turning into a function! Create a function to handle choosing
 	a random element from an array instead of doing the math each time.
 */
-var chosenMonster = monsters[Math.floor(Math.random() * monsters.length)];
-var chosenWeapon = weapons[Math.floor(Math.random() * weapons.length)];
-var chosenAction = actions[Math.floor(Math.random() * actions.length)];
 
-var output = "Armed with only their " +
-	chosenWeapon +
-	" our " +
-	heroGender +
-	" " +
-	chosenAction +
-	" the evil " +
-	chosenMonster +
-	"!";
+function randomize (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+function generateStory () {
+    var chosenMonster = randomize(monsters);
+    var chosenWeapon = randomize(weapons);
+    var chosenAction = randomize(actions);
 
-document.getElementById("adventure").innerText = output;
+    var output = "Armed with only their " + chosenWeapon + " our " + heroGender + " " +
+        chosenAction + " the evil " + chosenMonster + "!";
 
-/*
-	TODO: Right now the "new adventure!" button doesn't work. Create a function
-	to bind to that button so that the user can create a new adventure without
-	refreshing the page.
-*/
+    document.getElementById("adventure").innerText = output;
+}
+
+
+
+button.onclick = generateStory;
 
 /*
-	Optional: Create several more types of adventures, weapons, actions, etc.
 
 	Optional: Allow the user to interact with the story more than just clicking
 	the "new adventure!" button. You'll need to create variables to keep track
